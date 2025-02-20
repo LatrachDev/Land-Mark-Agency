ed3e0e000684d294281ec7babc99609050a44d16
LatrachDev <m.latrach.youcode@gmail.com>
2025-02-21 00:43:12 +0100

    LAN-1 update the video icon

--- resources/views/index.php
+++ resources/views/index.php
@@ -96,12 +96,8 @@
             />
             <!-- Play Button Overlay -->
             <div class="absolute inset-0 flex items-center justify-center">
-                <button class="w-16 h-16 rounded-full bg-black/30 flex items-center justify-center hover:bg-black/40 transition-all">
-                    <div class="w-12 h-12 rounded-full bg-white flex items-center justify-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z" />
-                        </svg>
-                    </div>
+                <button class="w-16 h-16 rounded-full flex items-center justify-center hover:bg-black/40 transition-all">
+                    <img src="LM/Services/videoIcon2.svg" alt="" class="w-12 h-12 rounded-full flex items-center justify-center">
                 </button>
             </div>
         </div>
