7106a7254a372da260547a495f232a385ab5a80d
LatrachDev <m.latrach.youcode@gmail.com>
2025-06-09 11:21:04 +0100

    LAN-23 implement the routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,12 @@
-import Promotion from './components/Promotion';
-import Nav from './components/Nav';
-import Hero from './components/Hero';
-import Mission from './components/Mission';
-import Services from './components/Services';
-import Projects from './components/Projects';
-import Content from './components/Content';
-import Reviews from './components/Reviews';
-import Contact from './components/Contact';
-import Blog from './components/Blog';
-import FAQ from './components/FAQ';
-import Footer from './components/Footer';
-import './App.css'
+import { BrowserRouter } from 'react-router-dom';
+import RoutesList from './Routes/Routes';
+import './App.css';
 
 function App() {
   return (
-    <div className="font-[Jost]">
-      {/* <Promotion /> */}
-      <div className="bg-[url('../assets/BG/Asset7.png')] w-full bg-cover bg-no-repeat">
-        <Nav />
-        {/* <Hero />
-        <Mission /> */}
-      </div>
-      {/* <Services />
-      <Projects />
-      <Content />
-      <Reviews />
-      <Contact />
-      <Blog />
-      <FAQ />
-      <Footer /> */}
-    </div>
+    <BrowserRouter>
+      <RoutesList />
+    </BrowserRouter>
   );
 }
 
--- frontend/src/Routes/Routes.jsx
+++ frontend/src/Routes/Routes.jsx
@@ -1,15 +1,17 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import About from '../pages/About';
-import Services from '../pages/Services';
+import ServicesPage from '../pages/ServicesPage';
 import Blog from '../pages/Blog';
+import Portfolio from '../pages/Portfolio';
 
 function RoutesList() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/portfolio" element={<Portfolio />} />
       <Route path="/about" element={<About />} />
-      <Route path="/services" element={<Services />} />
+      <Route path="/services" element={<ServicesPage />} />
       <Route path="/blog" element={<Blog />} />
     </Routes>
   );
